4d439faefb22870d9380794a897d04120b0d2424
Marcelo Penha <marcelopenhati@outlook.com>
2022-06-15 12:25:15 -0300

    add method to put string in upper case

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-public class Main{
-
-
-  public static void main(String args[]){
-    System.out.println("Main class of a project");
-  }
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Main class of a project");
+	}
+	public String transformString(String string){
+		return string.toUpperCase();
+	}
 }
