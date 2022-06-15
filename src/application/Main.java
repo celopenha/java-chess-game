588d1d0db7a0b0907c0f39d6dd4d8279cb6aa326
Marcelo Penha <marcelopenhati@outlook.com>
2022-06-15 12:45:44 -0300

    remove function tranformUpperCase

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,4 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Main class of a project");
 	}
-	public String transformString(String string){
-		return string.toUpperCase();
-	}
 }
