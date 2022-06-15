9e0988de65491aa9dcfaa6deea3309c9f3fcaa53
Marcelo Penha <marcelopenhati@outlook.com>
2022-06-15 12:31:18 -0300

    move to another folder

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-	public static void main(String args[]) {
-		System.out.println("Main class of a project");
-	}
-	public String transformString(String string){
-		return string.toUpperCase();
-	}
-}
